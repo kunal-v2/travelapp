239593474a954cdb89b2e2679f974da8da36bdf0
kunal chugh <157191243+kunal-v2@users.noreply.github.com>
2024-09-28 14:29:33 +0530

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,19 +1,38 @@
+import React, { useState } from "react";
 import '../component/Header.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav>
-      <div className="logo">
-        <img src={require('../assets/images/abc.png')} alt="logo" />
+      <div className="header-content">
+        <div className="logo">
+          <img src={require('../assets/images/abc.png')} alt="logo" />
+        </div>
+        <div className="CompanyName">
+          <h1>Mountaineers</h1>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          ☰
+        </div>
       </div>
-      <ul>
-        <li><b><a href="#Home">Home</a></b></li>
-        <li><b><a href="#Ticket">Ticket</a></b></li>
-        <li><b><a href="#Explore">Explore</a></b></li>
-        <li><b><a href="#Activity">Activity</a></b></li>
-        <li><b><Link to="/contact">Contact</Link></b></li> {/* Use Link */}      </ul>
-      <button><b>Login</b></button>
+
+      <div className={`menu-container ${isMenuOpen ? 'show-menu' : ''}`}>
+        <ul className="menu">
+          <li><b><a href="#Home">Home</a></b></li>
+          <li><b><a href="#Ticket">Ticket</a></b></li>
+          <li><b><a href="#Explore">Explore</a></b></li>
+          <li><b><a href="#Activity">Activity</a></b></li>
+          <li><b><Link to="/contact">Contact</Link></b></li>
+        </ul>
+        <button className="login-button"><b>Login</b></button>
+      </div>
     </nav>
   );
 }
